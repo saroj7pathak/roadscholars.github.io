a0d53c10c94886653aea40a13401262427ccfd55
Peter Newton <46580447+peter-newton@users.noreply.github.com>
2020-11-19 20:58:22 -1000

    Initial

---
+++ mixChart.js
@@ -0,0 +1,78 @@
+var labels = [
+    "Alabama",
+    "California",
+    "Georgia",
+    "Hawaii",
+    "Indiana",
+    "Kentucky",
+    "Montana"
+];
+var kesal = [
+    20000,
+    14000,
+    12000,
+    15000,
+    18000,
+    19000,
+    22000
+];
+var year = [
+    2019,
+    1989,
+    1994,
+    2014,
+    1993,
+    2018,
+    1996
+];
+var mix = document.getElementById("mixChart").getContext('2d');
+var mixChart = new Chart(mix, {
+    type: 'bar',
+    data: {
+        labels: labels,
+        datasets: [
+            {
+                type: 'line',
+                label: "kESAL",
+                data: kesal,
+                borderColor: 'rgba(75, 192, 192, 1)',
+                backgroundColor: 'rgba(0, 0, 0, 0)',
+                yAxisID: 'kESAL',
+            },
+            {
+                label: "Year",
+                data: year,
+                borderColor: 'rgba(0, 0, 0, 0)',
+                backgroundColor: 'rgba(192, 75, 192, 0.5)',
+                yAxisID: 'year',
+            }
+        ]
+    },
+    options: {
+        scales: {
+            yAxes: [
+                {
+                    id: "kESAL",
+                    ticks: {
+                        beginAtZero: true,
+                    },
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Kesal (U$)'
+                      }
+                },
+                {
+                    id: "year",
+                    position: 'right',
+                    ticks: {
+                        beginAtZero: false,
+                    },
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Year'
+                      }
+                },
+            ]
+        },
+    }
+});
---
+++ pieChart.js
@@ -0,0 +1,28 @@
+var labels = [
+    "I'm a lot of info",
+    "I am not",
+];
+var data = [
+    70,
+    30,
+];
+var pie = document.getElementById("pieChart").getContext('2d');
+var myChart = new Chart(pie, {
+    type: 'pie',
+    data: {
+        labels: labels,
+        datasets: [
+            {
+                data: data,
+                borderColor: ['rgba(75, 192, 192, 1)', 'rgba(192, 0, 0, 1)'],
+                backgroundColor: ['rgba(75, 192, 192, 0.2)', 'rgba(192, 0, 0, 0.2)'],
+            }
+        ]
+    },
+    options: {
+        title: {
+            display: true,
+            text: "American Roads"
+        }
+    }
+});
